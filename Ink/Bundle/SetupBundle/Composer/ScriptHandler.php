51bfd583b41be19c9d50aa13efe5a6d6973b81ba
Maxwell Vandervelde <mvanderv@nerdery.com>
2013-03-01 10:31:27 -0600

    Skip on existing parameters

--- Ink/Bundle/SetupBundle/Composer/ScriptHandler.php
+++ Ink/Bundle/SetupBundle/Composer/ScriptHandler.php
@@ -1,104 +1,178 @@
 <?php
-/**
- * ScriptHandler.php
+
+/*
+ * This file is part of the Symfony package.
  *
- * Post-install hooks for installing Symfony2 with composer
+ * (c) Fabien Potencier <fabien@symfony.com>
  *
- * @author    Maxwell Vandervelde <Max@MaxVandervelde.com>
- * @version   1.0.0
- * @copyright (c) 2013, Ink Applications
- * @license   http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
- *            Attribution-NonCommercial-ShareAlike 3.0 Unported
- *            Some Rights Reserved
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-namespace Ink\Bundle\SetupBundle\Composer;
+namespace Sensio\Bundle\DistributionBundle\Composer;
 
-use InvalidArgumentException;
-use RuntimeException;
-use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as SymfonyScriptHandler;
+use Symfony\Component\ClassLoader\ClassCollectionLoader;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
 
 /**
- * ScriptHandler
- *
- * Contains logic to set up post-install hooks for composer to use when setting
- * up Symfony2 with a distribution file set.
- *
- * @author Maxwell Vandervelde <Max@MaxVandervelde.com>
+ * @author Jordi Boggiano <j.boggiano@seld.be>
  */
-class ScriptHandler extends SymfonyScriptHandler
+class ScriptHandler
 {
-    /**
-     * Parameter Files
-     *
-     * @var array A list of the possible parameters distribution file names, in
-     *            The order that they should be looked for.
-     */
-    protected static $parameterFiles = array(
-        'parameters.dist.yml',
-        'parameters.dist.xml',
-        'parameters.dist.php',
-    );
-
-    /**
-     * Build Parameters
-     *
-     * This is a hook for composer to build before Symfony is setup.
-     * If no parameters file is included in the project, It copies a parameters
-     * distribution file in its place.
-     *
-     * @param  $event            The composer hook event
-     * @throws \RuntimeException Throws on copy fail
-     */
-    public static function buildParameters($event)
+    public static function buildBootstrap($event)
     {
-        echo 'Building Parameters File';
-        $options      = self::getOptions($event);
-        $appDir       = $options['symfony-app-dir'];
-        $distFile     = self::getParametersDistFile($appDir);
-        $distFileInfo = pathinfo($distFile);
-        $destination  = $appDir . DIRECTORY_SEPARATOR . 'config'
-            . DIRECTORY_SEPARATOR . 'parameters.' . $distFileInfo['extension'];
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
 
-        $copyStatus = copy($distFile, $destination);
+        if (!is_dir($appDir)) {
+            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not build bootstrap file.'.PHP_EOL;
 
-        if (!$copyStatus) {
-            throw new RuntimeException(
-                'Could not create parameters. File copy failed at: ' . $destination
-            );
+            return;
+        }
+
+        static::executeBuildBootstrap($appDir, $options['process-timeout']);
+    }
+
+    public static function clearCache($event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+
+        if (!is_dir($appDir)) {
+            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not clear the cache.'.PHP_EOL;
+
+            return;
+        }
+
+        static::executeCommand($event, $appDir, 'cache:clear --no-warmup', $options['process-timeout']);
+    }
+
+    public static function installAssets($event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+        $webDir = $options['symfony-web-dir'];
+
+        $symlink = '';
+        if ($options['symfony-assets-install'] == 'symlink') {
+            $symlink = '--symlink ';
+        } elseif ($options['symfony-assets-install'] == 'relative') {
+            $symlink = '--symlink --relative ';
+        }
+
+        if (!is_dir($webDir)) {
+            echo 'The symfony-web-dir ('.$webDir.') specified in composer.json was not found in '.getcwd().', can not install assets.'.PHP_EOL;
+
+            return;
+        }
+
+        static::executeCommand($event, $appDir, 'assets:install '.$symlink.escapeshellarg($webDir));
+    }
+
+    public static function installRequirementsFile($event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+
+        if (!is_dir($appDir)) {
+            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not install the requirements file.'.PHP_EOL;
+
+            return;
+        }
+
+        copy(__DIR__.'/../Resources/skeleton/app/SymfonyRequirements.php', $appDir.'/SymfonyRequirements.php');
+        copy(__DIR__.'/../Resources/skeleton/app/check.php', $appDir.'/check.php');
+
+        $webDir = $options['symfony-web-dir'];
+
+        if (is_file($webDir.'/config.php')) {
+            copy(__DIR__.'/../Resources/skeleton/web/config.php', $webDir.'/config.php');
         }
     }
 
-    /**
-     * Get Parameters File
-     *
-     * Gets the appropriate parameters distribution file for the Symfony2
-     * application based on the first available file
-     * as defined in static::$parameterFiles
-     *
-     * @see    ScriptHandler::$parameterFiles
-     * @param  $appDir string            The application directory of Symfony2
-     * @return string                    The Parameters Distribution file to use
-     * @throws \RuntimeException         Thrown when no dist file is found
-     * @throws \InvalidArgumentException Thrown on invalid input
-     */
-    public static function getParametersDistFile($appDir)
+    public static function doBuildBootstrap($appDir)
     {
-        if (!is_string($appDir)) {
-            throw new InvalidArgumentException(
-                'First parameter expected a path string'
-            );
+        $file = $appDir.'/bootstrap.php.cache';
+        if (file_exists($file)) {
+            unlink($file);
         }
 
-        $filePrefixPath = $appDir . DIRECTORY_SEPARATOR . 'config';
+        ClassCollectionLoader::load(array(
+                'Symfony\\Component\\DependencyInjection\\ContainerAwareInterface',
+                // Cannot be included because annotations will parse the big compiled class file
+                //'Symfony\\Component\\DependencyInjection\\ContainerAware',
+                'Symfony\\Component\\DependencyInjection\\Container',
+                'Symfony\\Component\\HttpKernel\\Kernel',
+                'Symfony\\Component\\ClassLoader\\ClassCollectionLoader',
+                'Symfony\\Component\\ClassLoader\\ApcClassLoader',
+                'Symfony\\Component\\HttpKernel\\Bundle\\Bundle',
+                'Symfony\\Component\\Config\\ConfigCache',
+                'Symfony\\Bundle\\FrameworkBundle\\HttpKernel',
+                // cannot be included as commands are discovered based on the path to this class via Reflection
+                //'Symfony\\Bundle\\FrameworkBundle\\FrameworkBundle',
+            ), dirname($file), basename($file, '.php.cache'), false, false, '.php.cache');
 
-        foreach (static::$parameterFiles as $file) {
-            $filePath = $filePrefixPath . DIRECTORY_SEPARATOR . $file;
-            if (file_exists($filePath)) {
-                return $filePath;
-            }
+        file_put_contents($file, sprintf("<?php
+
+namespace { \$loader = require_once __DIR__.'/autoload.php'; }
+
+%s
+
+namespace { return \$loader; }
+            ", substr(file_get_contents($file), 5)));
+    }
+
+    protected static function executeCommand($event, $appDir, $cmd, $timeout = 300)
+    {
+        $php = escapeshellarg(self::getPhp());
+        $console = escapeshellarg($appDir.'/console');
+        if ($event->getIO()->isDecorated()) {
+            $console.= ' --ansi';
         }
 
-        throw new RuntimeException('Could not find parameters dist file');
+        $process = new Process($php.' '.$console.' '.$cmd, null, null, null, $timeout);
+        $process->run(function ($type, $buffer) { echo $buffer; });
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd)));
+        }
+    }
+
+    protected static function executeBuildBootstrap($appDir, $timeout = 300)
+    {
+        $php = escapeshellarg(self::getPhp());
+        $cmd = escapeshellarg(__DIR__.'/../Resources/bin/build_bootstrap.php');
+        $appDir = escapeshellarg($appDir);
+
+        $process = new Process($php.' '.$cmd.' '.$appDir, null, null, null, $timeout);
+        $process->run(function ($type, $buffer) { echo $buffer; });
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException('An error occurred when generating the bootstrap file.');
+        }
+    }
+
+    protected static function getOptions($event)
+    {
+        $options = array_merge(array(
+                'symfony-app-dir' => 'app',
+                'symfony-web-dir' => 'web',
+                'symfony-assets-install' => 'hard'
+            ), $event->getComposer()->getPackage()->getExtra());
+
+        $options['symfony-assets-install'] = getenv('SYMFONY_ASSETS_INSTALL') ?: $options['symfony-assets-install'];
+
+        $options['process-timeout'] = $event->getComposer()->getConfig()->get('process-timeout');
+
+        return $options;
+    }
+
+    protected static function getPhp()
+    {
+        $phpFinder = new PhpExecutableFinder;
+        if (!$phpPath = $phpFinder->find()) {
+            throw new \RuntimeException('The php executable could not be found, add it to your PATH environment variable and try again');
+        }
+
+        return $phpPath;
     }
 }
